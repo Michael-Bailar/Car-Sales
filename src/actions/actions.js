4a6ea3367697ace15454b569b70c12264eae7a7d
Michael-B1764 <mikebailar@gmail.com>
2020-03-17 16:53:19 -0700

    added the BUY_ITEM feature. Now to remove those items.

---
+++ src/actions/actions.js
@@ -0,0 +1,10 @@
+export const REMOVE_FEATURE = 'REMOVE_FEATURE'
+export const BUY_ITEM = 'BUY_ITEM'
+
+export const removeFeature = (feature) => {
+    return { type: REMOVE_FEATURE, payload: feature}
+}
+
+export const buyItem = (feature) => {
+    return { type: BUY_ITEM, payload: feature, id: feature.id}
+}
---
+++ src/reducers/reducer.js
@@ -0,0 +1,43 @@
+import { BUY_ITEM, REMOVE_FEATURE } from '../actions/actions'
+
+export const initialState = {
+    additionalPrice: 0,
+    car: {
+      price: 26395,
+      name: '2019 Ford Mustang',
+      image:
+        'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
+      features: []
+    },
+    additionalFeatures: [
+      { id: 1, name: 'V-6 engine', price: 1500 },
+      { id: 2, name: 'Racing detail package', price: 1500 },
+      { id: 3, name: 'Premium sound system', price: 500 },
+      { id: 4, name: 'Rear spoiler', price: 250 }
+    ]
+  };
+
+export const reducer = (state = initialState, action) => {
+    console.log(state, action)
+    switch(action.type) {
+      case BUY_ITEM:
+        return {
+          ...state,
+          additionalPrice: state.additionalPrice + action.payload.price,
+          car: {
+            ...state.car,
+            features: [...state.car.features, action.payload]
+          },
+          additionalFeatures: state.additionalFeatures.filter(item => {
+            if (item.id !== action.id) {
+              return{...item}
+            }
+          })
+        }
+      case REMOVE_FEATURE:
+        console.log('working') 
+        return state
+      default:
+          return state
+    }
+}
